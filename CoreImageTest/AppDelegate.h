94a60946aed8eda531287104dc165817f1a24a9d
LGM <LGM@GM.local>
2015-06-24 16:38:29 +0800

    Initial Commit

---
+++ CoreImageTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreImageTest
+//
+//  Created by LGM on 15/6/24.
+//  Copyright (c) 2015年 GM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreImageTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreImageTest
+//
+//  Created by LGM on 15/6/24.
+//  Copyright (c) 2015年 GM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
